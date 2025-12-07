8a747d84a04ecda27c2c5df9269cd188144de252
Alvarohl15 <ahuertalvaro@ciencias.unam.mx>
2025-12-07 16:36:21 -0600

    slide bar

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ import pandas as pd
 import math
 from pathlib import Path
 
+#booleano para definir si es portafolio arbitrario
+arbitrario=False
+
+#Etiquetas para pesos de los ETFs
+W_EEM=0.0
+W_EWC=0.0
+W_IEUR=0.0
+W_SPLG=0.0
+W_EWJ=0.0
+W_XLC=0.0
+W_XLY=0.0
+W_XLP=0.0
+W_XLE=0.0
+W_XLF=0.0
+W_XLV=0.0
+W_XLI=0.0
+W_XLB=0.0
+W_XLRE=0.0
+W_XLK=0.0
+W_XLU=0.0
+
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='SF-Portafolio de Inversión',
@@ -33,13 +55,80 @@ with st.sidebar:
     st.header("Definición de Pesos de los Activos")
     st.subheader("ETFs Regionales")
 
-    left, right = st.columns([2,1])
+    left, right = st.columns([2,1],vertical_alignment='center')
     with left:
         st.write("SPLG: ETF que sigue el índice S&P 500.")
         st.write("EWC: ETF que sigue el índice de acciones canadienses.")
         st.write("IEUR: ETF que sigue el índice de acciones europeas.")    
         st.write("EEM: ETF que sigue el índice de acciones de mercados emergentes.")
         st.write("EWJ: ETF que sigue el índice de acciones japonesas.")
+    
+    if arbitrario:
+        #######Input de pesos para portafolio arbitrario########
+        with right:
+            st.number_input(
+                "Peso asignado",
+                min_value=0.0,
+                max_value=100.0,
+                value=0.0,
+                step=0.1,
+                key="W_SPLG",
+            )
+            st.number_input(
+                "Peso asignado",
+                min_value=0.0,
+                max_value=100.0,
+                value=0.0,
+                step=0.1,
+                key="W_EWC",
+            )
+            st.number_input(
+                "Peso asignado",
+                min_value=0.0,
+                max_value=100.0,
+                value=0.0,
+                step=0.1,
+                key="W_IEUR",
+            )
+            st.number_input(
+                "Peso asignado",
+                min_value=0.0,
+                max_value=100.0,
+                value=0.0,
+                step=0.1,
+                key="W_EEM",
+            )
+            st.number_input(
+                "Peso asignado",
+                min_value=0.0,
+                max_value=100.0,
+                value=0.0,
+                step=0.1,
+                key="W_EWJ",
+            )
+    else:
+        #########etiquetas para peso de portafolio optimizado y black-litterman######
+        st.text_input(W_SPLG)
+        st.text_input(W_EWC)        
+        st.text_input(W_IEUR)
+        st.text_input(W_EEM)
+        st.text_input(W_EWJ)
+
+    st.subheader("ETFs Sectoriales")
+
+    with left:
+        st.write("XLC: ETF que sigue el índice de comunicaciones.")
+        st.write("XLY: ETF que sigue el índice de consumo discrecional.")
+        st.write("XLP: ETF que sigue el índice de consumo básico.")    
+        st.write("XLE: ETF que sigue el índice de energía.")
+        st.write("XLF: ETF que sigue el índice financiero.")
+        st.write("XLV: ETF que sigue el índice de salud.")
+        st.write("XLI: ETF que sigue el índice industrial.")
+        st.write("XLB: ETF que sigue el índice de materiales.")
+        st.write("XLRE: ETF que sigue el índice inmobiliario.")
+        st.write("XLK: ETF que sigue el índice tecnológico.")
+        st.write("XLU: ETF que sigue el índice de servicios públicos.")
+    
     
     with right:
         st.number_input(
@@ -48,7 +137,7 @@ with st.sidebar:
             max_value=100.0,
             value=0.0,
             step=0.1,
-            key="W_SPLG",
+            key="W_XLC",
         )
         st.number_input(
             "Peso asignado",
@@ -56,7 +145,7 @@ with st.sidebar:
             max_value=100.0,
             value=0.0,
             step=0.1,
-            key="W_EWC",
+            key="W_XLY",
         )
         st.number_input(
             "Peso asignado",
@@ -64,7 +153,7 @@ with st.sidebar:
             max_value=100.0,
             value=0.0,
             step=0.1,
-            key="W_IEUR",
+            key="W_XLP",
         )
         st.number_input(
             "Peso asignado",
@@ -72,7 +161,7 @@ with st.sidebar:
             max_value=100.0,
             value=0.0,
             step=0.1,
-            key="W_EEM",
+            key="W_XLE",
         )
         st.number_input(
             "Peso asignado",
@@ -80,141 +169,66 @@ with st.sidebar:
             max_value=100.0,
             value=0.0,
             step=0.1,
-            key="W_EWJ",
+            key="W_XLF",
         )
-    
-    st.subheader("ETFs Sectoriales")
-
-    '''
-    XLC: ETF que sigue el índice de comunicaciones.
-    '''
-    W_XLC = st.number_input(
-        "Peso asignado a ETF XLC:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLY: ETF que sigue el índice de consumo discrecional.
-    '''
-    W_XLY = st.number_input(
-        "Peso asignado a ETF V:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLP: ETF que sigue el índice de consumo básico.
-    '''
-    W_XLP = st.number_input(
-        "Peso asignado a ETF XLP:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLE: ETF que sigue el índice de energía.
-    '''
-    W_XLE = st.number_input(
-        "Peso asignado a ETF XLE:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLF: ETF que sigue el índice financiero.
-    '''
-    W_XLF = st.number_input(
-        "Peso asignado a ETF XLF:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLV: ETF que sigue el índice de salud.
-    '''
-    W_XLV = st.number_input(
-        "Peso asignado a ETF XLV:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLI: ETF que sigue el índice industrial.
-    '''
-    W_XLI = st.number_input(
-        "Peso asignado a ETF XLI:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLB: ETF que sigue el índice de materiales.
-    '''
-    W_XLB = st.number_input(
-        "Peso asignado a ETF XLB:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLRE: ETF que sigue el índice inmobiliario.
-    '''
-    W_XLRE = st.number_input(
-        "Peso asignado a ETF XLRE:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLK: ETF que sigue el índice tecnológico.
-    '''
-    W_XLK = st.number_input(
-        "Peso asignado a ETF XLK:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
-
-    '''
-    XLU: ETF que sigue el índice de servicios públicos.
-    '''
-    W_XLU = st.number_input(
-        "Peso asignado a ETF XLU:",
-        min_value=0.0,
-        max_value=100.0,
-        value=0.0,
-        step=0.1,
-    )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLV",
+        )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLI",
+        )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLB",
+        )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLRE",
+        )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLK",
+        )
+        st.number_input(
+            "Peso asignado",
+            min_value=0.0,
+            max_value=100.0,
+            value=0.0,
+            step=0.1,
+            key="W_XLU",
+        )
 
 
 
 ########################## Análisis de Portafolio Arbitrario ##########################
 if tipo_portafolio == "Arbitrario":
+    Arbitrario=True
     st.subheader("Análisis de Portafolio Arbitrario")
     '''
     Por favor, defina el peso de cada uno de los activos que componen el portafolio.
     '''
-    
 
     weight_matrix_regionales = pd.DataFrame(
     {
@@ -233,3 +247,5 @@ if tipo_portafolio == "Arbitrario":
     )
 
     st.table(weight_matrix_regionales)
+else:
+    Arbitrario=False
